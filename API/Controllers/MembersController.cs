fa109eae3148f9f2a6fee8b907909b2ed9e8e399
Rama Ahmad Rimawi <ramaahmad5533@gmail.com>
2026-08-01 00:24:11 +0300

    section4

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,14 +1,14 @@
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")] //we use=> loclalhast:5001/api/members
-    [ApiController]
-    public class MembersController(AppDbContext context):ControllerBase
+   
+
+    public class MembersController(AppDbContext context):BaseApiController
     {
         [HttpGet]
         public async Task< ActionResult<IReadOnlyList<AppUser>>> GetMembers()
@@ -16,6 +16,9 @@ namespace API.Controllers
             var members=await context.Users.ToListAsync();
             return members;
         }
+
+         [Authorize]
+
         [HttpGet("{id}")]  //we use=> loclalhast:5001/api/members/bob-id
         public async Task< ActionResult<AppUser>> GetMember(string id)
         {
